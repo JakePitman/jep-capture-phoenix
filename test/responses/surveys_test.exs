c7622b1182881acfd3eccd855c825d4d52e5a5d4
Jake Pitman <jakeseanp@gmail.com>
2019-09-10 14:40:03 +1000

    add test for values_tally with question_id

--- test/responses/surveys_test.exs
+++ test/responses/surveys_test.exs
@@ -36,12 +36,14 @@ defmodule Capture.SurveysTest do
   describe "values_tally" do
     @response_1 %{"question_id" => 1, "survey_id" => 1, "user_id" => 1,"value" => 3}
     @response_2 %{"question_id" => 1, "survey_id" => 1, "user_id" => 2,"value" => 3}
-    @response_3 %{"question_id" => 2, "survey_id" => 2, "user_id" => 1,"value" => 3}
+    @response_3 %{"question_id" => 2, "survey_id" => 1, "user_id" => 2,"value" => 3}
+    @response_4 %{"question_id" => 2, "survey_id" => 2, "user_id" => 1,"value" => 3}
 
     setup do
       response_1 = %Response{} |> Response.changeset(@response_1) |> Repo.insert!
       response_2 = %Response{} |> Response.changeset(@response_2) |> Repo.insert!
       response_3 = %Response{} |> Response.changeset(@response_3) |> Repo.insert!
+      response_4 = %Response{} |> Response.changeset(@response_4) |> Repo.insert!
       # I don't want this thing :/
       {:ok, response_1: response_1}
     end
@@ -50,35 +52,20 @@ defmodule Capture.SurveysTest do
       assert Surveys.values_tally(%{"survey_id" => 1}) == %{
           ones: 0,
           twos: 0,
+          threes: 3,
+          fours: 0,
+          fives: 0,
+        }
+    end
+
+    test "counts the number of responses for each value, for given survey_id and question_id" do
+      assert Surveys.values_tally(%{"survey_id" => 1, "question_id" => 1}) == %{
+          ones: 0,
+          twos: 0,
           threes: 2,
           fours: 0,
           fives: 0,
         }
     end
   end
-
-  # describe "question_answers" do
-  #   @response_1 %{"question_id" => 1, "survey_id" => 1, "user_id" => 1,"value" => 3}
-  #   @response_2 %{"question_id" => 1, "survey_id" => 1, "user_id" => 2,"value" => 3}
-  #   @response_3 %{"question_id" => 2, "survey_id" => 1, "user_id" => 1,"value" => 3}
-  #   @response_4 %{"question_id" => 3, "survey_id" => 2, "user_id" => 1,"value" => 3}
-
-  #   setup do
-  #     response_1 = %Response{} |> Response.changeset(@response_1) |> Repo.insert!
-  #     response_2 = %Response{} |> Response.changeset(@response_2) |> Repo.insert!
-  #     response_3 = %Response{} |> Response.changeset(@response_3) |> Repo.insert!
-  #     response_4 = %Response{} |> Response.changeset(@response_4) |> Repo.insert!
-  #     {:ok, response_1: response_1}
-  #   end
-
-  #   test "counts the number of responses for each value, for given question_id" do
-  #     assert Surveys.question_answers(1) == %{
-  #         ones: 0,
-  #         twos: 0,
-  #         threes: 2,
-  #         fours: 0,
-  #         fives: 0,
-  #       }
-  #   end
-  # end
 end
